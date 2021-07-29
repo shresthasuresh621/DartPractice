3e9971f31cfc955de16bc247456885a031380608
Suresh Shrestha <shresthasuresh621@gmail.com>
2021-07-30 00:42:58 +0545

    singleLine amd Multiline comments Eg.

---
+++ bin/2_comments.dart
@@ -0,0 +1,7 @@
+void main (){
+  print('this is the just the example of singe line comment and multiple lin e comment');
+  print('comments');
+
+  //this the example of single line comment.
+  /* Multiline comment*/
+}
